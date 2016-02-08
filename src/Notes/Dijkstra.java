bc05d5ce304c08249f05203ddcbb120e050218b2
Willy Yang <tinysqueak@gmail.com>
2016-02-08 10:04:38 -0800

    Need to fix bug with adding distances when reached last node

--- src/Notes/Dijkstra.java
+++ src/Notes/Dijkstra.java
@@ -81,7 +81,7 @@ public class Dijkstra implements Shorty {
 
 		initializePv(nodeDistances.size());
 
-		System.out.println(dv);
+		//System.out.println(dv);
 
 
 	}
@@ -157,12 +157,11 @@ public class Dijkstra implements Shorty {
 		double nextMinDistance = Double.POSITIVE_INFINITY;
 		String nextStartNode = "";
 
+		kMap.put(startNode, true);
+
 		if(kMap.containsValue(false)) {
 
-			kMap.put(startNode, true);
-
-			ArrayList<String> distanceKeys = new ArrayList<String>(nodeDistances.get(startNode).keySet());
-
+			ArrayList<String> distanceKeys = new ArrayList<String>(nodeDistances.get(startNode).keySet());			
 			System.out.println("Nodes connected to " + startNode + ": " + distanceKeys);
 
 			/*double minDistance = nodeDistances.get(startNode).get(distanceKeys.get(0)) + startDistance;
@@ -170,8 +169,11 @@ public class Dijkstra implements Shorty {
 
 			for(int i = 0; i < nodeDistances.get(startNode).size(); i++) {
 
-				dv.set(nodeIndices.indexOf(distanceKeys.get(i)), nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance);
-
+				//if((nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance) < dv.get(nodeIndices.indexOf(distanceKeys.get(i)))) {
+					
+					dv.set(nodeIndices.indexOf(distanceKeys.get(i)), nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance);
+					
+				//}
 				/*minDistance = (minDistance < nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance)
 					? minDistance : nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance;
 			nextStartNode = (minDistance < nodeDistances.get(startNode).get(distanceKeys.get(i)) + startDistance)
@@ -194,22 +196,27 @@ public class Dijkstra implements Shorty {
 					nextStartNode = nodeIndices.get(i);
 
 				}
-			}
 
+			}
+			
 			System.out.println("Next start node is: " + nextStartNode);
 			System.out.println("Next minDistance is: " + nextMinDistance);
+
+			//need to create path recursively based on previous node, current implementation
+			//won't work for more complicated graphs
 			path.put(startNode, nextStartNode);
 
 			//path doesn't print out correctly for Graph3 sample graph?
 			System.out.println("Path is: " + path);
 
+			System.out.println("Distances are: " + dv);
+			System.out.println("Previous nodes are: " + pv);
+			System.out.println("Visited Nodes: " + kMap);
+
+			System.out.println("kMap contains false: " + kMap.containsValue(false));
+
 			System.out.println();
 
-			System.out.println(dv);
-			System.out.println(pv);
-			System.out.println(kMap);
-
-			System.out.println();
 		}
 
 		if(kMap.containsValue(false)) {
